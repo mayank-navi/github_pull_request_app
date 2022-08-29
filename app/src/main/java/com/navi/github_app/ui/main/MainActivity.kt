10bff7042518251cac0d4092f296dff997c1f657
mayank-navi <mayank.sheoran@navi.com>
2022-08-29 13:17:53 +0530

    refactored1.0

--- app/src/main/java/com/navi/github_app/ui/main/MainActivity.kt
+++ app/src/main/java/com/navi/github_app/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.navi.github_app.ui.main
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -13,41 +14,45 @@ import com.google.android.material.textfield.TextInputEditText
 import com.navi.github_app.R
 import com.navi.github_app.data.model.PullRequest
 import com.navi.github_app.data.repository.GithubApiRepository
+import com.navi.github_app.databinding.HomeScreenBinding
 import com.navi.github_app.ui.HomeScreen.PullRequestAdapter
 
 class MainActivity : AppCompatActivity() {
     private var prData = ArrayList<PullRequest>()
     private lateinit var prAdapter: PullRequestAdapter
+    private lateinit var binding: HomeScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_screen)
+        binding = HomeScreenBinding.inflate(layoutInflater)
+        val homeView = binding.root
+        setContentView(homeView)
         setupUI()
     }
 
     @SuppressLint("CutPasteId")
     private fun setupPullRequestRV() {
-        val rvPullRequest: RecyclerView = findViewById(R.id.rv_pull_requests)
+        val rvPullRequest: RecyclerView = binding.rvPullRequests
         val layoutManager = LinearLayoutManager(applicationContext)
         rvPullRequest.layoutManager = layoutManager
         rvPullRequest.itemAnimator = DefaultItemAnimator()
         prAdapter = PullRequestAdapter(prData)
         rvPullRequest.adapter = prAdapter
-        findViewById<RecyclerView>(R.id.rv_pull_requests).visibility = View.GONE
+        rvPullRequest.visibility = View.GONE
     }
 
     private fun setupUI () {
         setupPullRequestRV()
-        findViewById<Button>(R.id.fetch_button).setOnClickListener {
-            val repoName = findViewById<TextInputEditText>(R.id.repo_name_input).text.toString()
-            val userName = findViewById<TextInputEditText>(R.id.user_name_input).text.toString()
+        binding.fetchButton.setOnClickListener {
+            val repoName = binding.repoNameInput.text.toString()
+            val userName = binding.userNameInput.text.toString()
             onFetchPress(userName, repoName)
         }
     }
 
     private fun onFetchPress(userName: String?, repoName: String?) {
-        val noDataTv: TextView = findViewById(R.id.tv_error_when_no_user_inputs)
-        val prRv: RecyclerView = findViewById(R.id.rv_pull_requests)
+        val noDataTv: TextView = binding.tvErrorWhenNoUserInputs
+        val prRv: RecyclerView = binding.rvPullRequests
         GithubApiRepository().getAllClosedPullRequests(prRv, noDataTv, userName, repoName)
     }
 }
